38e68a565bc65471633aad1bee43dd573a1a8560
DeadlyPho <jasontang.exe@gmail.com>
2019-04-27 15:14:11 -0700

    Commiting my changes before pulling

--- src/script.js
+++ src/script.js
@@ -1,44 +1,64 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+'use strict'
 
 
 
 
 
 document.addEventListener('DOMContentLoaded', function () {
-	let sortedArray= [{"name":"ay","contents":"aylmao","rating":12,"author":"author???","tag":"meh",},{"name":"ay2","contents":"aylmao2","rating":3,"author":"author???2","tag":"meh2",}]
-//drawFeed(sortedArray){
-	var canvas = document.getElementById("myCanvas");
-	var ctx = canvas.getContext("2d");
-	let iterX=0;
-	let iterY=30;
+	let posts= feed()
+	let sortedArray = sortFeed(posts);
+	//var canvas = document.getElementById("myCanvas");
+	//var ctx = canvas.getContext("2d");
+
+	let padding=10;
 	for(let i=0;i<sortedArray.length;i=i+1){
+		//var Mycanvas = document.getElementById("myCanvas");
+		var canvas=document.createElement("CANVAS");
+		canvas.id="post"+i;
+		let iterX=20;
+		let iterY=30;
+		//canvas.className  = "myClass";
+		//canvas.id = "myId";
+		//canvas.width=100;
+		//canvas.height=100;
+		var ctx = canvas.getContext("2d");
+		ctx.rect(iterX-5, iterY-10, 100, 55);
+		ctx.stroke();
 		ctx.font = "10px Arial";
-		ctx.fillText(sortedArray[i].name, iterX, iterY);
+		ctx.fillText("Title: "+sortedArray[i].name, iterX, iterY);
 		iterY=iterY+10;
-		ctx.fillText(sortedArray[i].contents, iterX, iterY);
+		ctx.fillText("Content: "+sortedArray[i].contents, iterX, iterY);
 		iterY=iterY+10;
-		ctx.fillText(sortedArray[i].rating, iterX, iterY);
+		ctx.fillText("Rating: "+sortedArray[i].rating, iterX, iterY);
 		iterY=iterY+10;
-		ctx.fillText(sortedArray[i].author, iterX, iterY);
+		ctx.fillText("Author: "+sortedArray[i].author, iterX, iterY);
 		iterY=iterY+10;
-		ctx.fillText(sortedArray[i].tag, iterX, iterY);
-		iterY=iterY+10;
+		ctx.fillText("Tag: "+sortedArray[i].tag, iterX, iterY);
+		iterY=iterY+10+padding;
+		//document.getElementsByTagName('body')[0].appendChild(canvas);
+		document.body.appendChild(canvas)
+				
 	}
 	
 }
 );
+
+function sortFeed(postList){
+	let posts = postList.sort(function(a, b){return b.rating-a.rating});
+	return posts;
+}
+
+
+function feed(){
+	let postList = getPosts();
+	return sortFeed(postList);
+}
+
+
+
+function getPosts(){
+	return [{"name":"ay","contents":"aylmao","rating":3,"author":"author???","tag":"meh",},{"name":"ay2","contents":"aylmao2","rating":123,"author":"author???2","tag":"meh2",}]
+}
+
+
+
