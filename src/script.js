68f4599e0ccc2c8d859441a21207659a76106db0
DeadlyPho <jasontang.exe@gmail.com>
2019-04-27 17:52:09 -0700

    My changes for the buttons

--- src/script.js
+++ src/script.js
@@ -1,9 +1,68 @@
 'use strict'
 
+document.addEventListener('DOMContentLoaded', function () {
+	let down1=true;
+	let up1=true;
+	let up2=true;
+	let down2=true;
+document.getElementById("upvote1").addEventListener("click", function(){if(up1&&down1){
+	let variable=(parseInt((document.getElementById("value1")).innerHTML)+1).toString();
+  document.getElementById("value1").innerHTML =variable;
+}
+else if(up1&&!down1){
+  let variable=(parseInt((document.getElementById("value1")).innerHTML)+2).toString();
+  document.getElementById("value1").innerHTML =variable;
+  down1=true;
+}
+  up1=false;
+}
+);
+document.getElementById("upvote2").addEventListener("click", function(){if(up2&&down2){
+	let variable=(parseInt((document.getElementById("value2")).innerHTML)+1).toString();
+  document.getElementById("value2").innerHTML =variable;
+}
+  else if(up2&&!down2){
+  let variable=(parseInt((document.getElementById("value2")).innerHTML)+2).toString();
+  document.getElementById("value2").innerHTML =variable;
+  down2=true;
+}
+  up2=false;
+}
+
+);
+document.getElementById("downvote1").addEventListener("click", function(){if(down1&&up1){
+	let variable=(parseInt((document.getElementById("value1")).innerHTML)-1).toString();
+  document.getElementById("value1").innerHTML =variable;
+  up1=true;
+}
+  else if(down1&&!up1){
+  let variable=(parseInt((document.getElementById("value1")).innerHTML)-2).toString();
+  document.getElementById("value1").innerHTML =variable;
+  up1=true;
+}
+down1=false;
+}
+);
+document.getElementById("downvote2").addEventListener("click", function(){if(down2&&up2){
+	let variable=(parseInt((document.getElementById("value2")).innerHTML)-1).toString();
+  document.getElementById("value2").innerHTML =variable;
+
+}
+
+  else if(down2&&!up2){
+  let variable=(parseInt((document.getElementById("value2")).innerHTML)-2).toString();
+  document.getElementById("value2").innerHTML =variable;
+  up2=true;
+}
+  down2=false;
+}
+);
 
 
 
-
+}
+);
+/*
 document.addEventListener('DOMContentLoaded', function () {
 	let posts= feed()
 	let sortedArray = sortFeed(posts);
@@ -42,7 +101,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	
 }
 );
-
+*/
 function sortFeed(postList){
 	let posts = postList.sort(function(a, b){return b.rating-a.rating});
 	return posts;
