357723d625be576421e20ff94ea06f0dc38db4a9
DeadlyPho <jasontang.exe@gmail.com>
2019-04-28 13:01:14 -0700

    Documentation

--- src/script.js
+++ src/script.js
@@ -1,74 +1,75 @@
 'use strict'
-	var posts= [{"name":"Educational Resources","contents":"Five professors of San Jose State University have proposed a new tutoring program for low-income and homeless students.","rating":3,"author":"author???","tag":"meh","time": new Date('Sat Apr 27 2019 7:51:16 GMT-0700 (Pacific Daylight Time)'),},
-	{"name":"New Options for Food","contents":"Two new food banks have opened up near San Jose State University;  one in 7th Street an one in San Fernando Street.","rating":123,"author":"author???2","tag":"meh2","time":new Date('Sat Apr 27 2019 11:51:16 GMT-0700 (Pacific Daylight Time)'),},
-	{"name":"Open Clinic","contents":"Clinic offers free healthcare to homeless in San Jose area","rating":700,"author":"author???2","tag":"meh2","time":new Date('Fri Apr 26 2019 11:51:16 GMT-0700 (Pacific Daylight Time)'),}];
-
+	//on startup
+	var tagAmount=6; //hardcoded amount of tags
 document.addEventListener('DOMContentLoaded', function () {
-	let up1=true;
+	
+	//for postpAGE
+	//runs the createPost
+	//todo get createpost to update posts in server memory
+	document.getElementById("submitNewPost").addEventListener("click", function(){
+	let newPost=createPost();
+	console.log(newPost);
+	pushPost(newPost);
+	}
+	);
+	
+	
+	//values for upvote and downvote logic
+	let up1=true; 
 	let down1=true;
 	let up2=true;
 	let down2=true;
+	
+	//initializes generated posts and sorted array for future sorting
 	let sortedArray;
+	let posts=getPosts();
+	
+	//uses pageType to select which version of script to run
+	//todo split into seperate script files
 	var pageType = document.getElementById('type');
+	
+	//if the pageType exists
+	// only trending and newst have a type element
+	//todo: add type to all pages
 	if(pageType !== null)
 	{
 		let index = 0;
+		
+		//runs two different sorting algorithms for the two pages
 		if(pageType.className === 'trending')
 		{
-			//alert(JSON.stringify(posts));
 			sortedArray = sortFeedRate(posts);
-			//alert(JSON.stringify(sortedArray));
 		}
 		else if(pageType.className === 'newest')
 		{
-			//alert(pageType.className);
 			sortedArray = sortFeedTime(posts);
 		}
 		
+		//if the array has been sorted run for loop to present the first two posts
+		//two slots for posts have been initialized in html, this updates those
 		if(sortedArray !== undefined){
-			//var canvas = document.getElementById("myCanvas");
-			//var ctx = canvas.getContext("2d");
-		/*
-			let padding=10;
-			for(let i=0;i<sortedArray.length;i=i+1){
-				//var Mycanvas = document.getElementById("myCanvas");
-				var canvas=document.createElement("CANVAS");
-				canvas.id="post"+i;
-				let iterX=20;
-				let iterY=30;
-				//canvas.className  = "myClass";
-				//canvas.id = "myId";
-				//canvas.width=100;
-				//canvas.height=100;
-				var ctx = canvas.getContext("2d");
-				ctx.rect(iterX-5, iterY-10, 100, 55);
-				ctx.stroke();
-				ctx.font = "10px Arial";
-				ctx.fillText("Title: "+sortedArray[i].name, iterX, iterY);
-				iterY=iterY+10;
-				ctx.fillText("Content: "+sortedArray[i].contents, iterX, iterY);
-				iterY=iterY+10;
-				ctx.fillText("Rating: "+sortedArray[i].rating, iterX, iterY);
-				iterY=iterY+10;
-				ctx.fillText("Author: "+sortedArray[i].author, iterX, iterY);
-				iterY=iterY+10;
-				ctx.fillText("Tag: "+sortedArray[i].tag, iterX, iterY);
-				iterY=iterY+10+padding;
-				//document.getElementsByTagName('body')[0].appendChild(canvas);
-				document.body.appendChild(canvas)*/
 			for(let i=0;i<2;i=i+1){
-				//alert(JSON.stringify(sortedArray[i]));
+				
+				//changging inner html
 				document.getElementById("name" + (i+1)).innerHTML = sortedArray[i].name + '<br />';
 				document.getElementById("content" + (i+1)).innerHTML = sortedArray[i].contents + '<br />';
 				document.getElementById("rating" + (i+1)).innerHTML = sortedArray[i].rating;
 				document.getElementById("date" + (i+1)).innerHTML = sortedArray[i].time.toLocaleString();
 				index++;
 			}
+			
+			//event listeners for upvote and downvote
 			document.getElementById("upvote1").addEventListener("click", function(){if(up1&&down1){
-				let variable=(parseInt((document.getElementById("rating1")).innerHTML)+1).toString();
-			  document.getElementById("rating1").innerHTML =variable;
-			  sortedArray[index - 2].rating++;
+			//todo replace document parsing for variable with array parsing for rating value
+			let variable=(parseInt((document.getElementById("rating1")).innerHTML)+1).toString();//finds and calculates new value (first click) stores as string
+			  document.getElementById("rating1").innerHTML =variable;//updates inner html
+			  sortedArray[index - 2].rating++; //updates rating value stored in array 
+			  
+			  
 			}
+			
+			//if the downvote has been clicked, simulate unclicking downvote and clicking the upvote
+			//increment upwards twice
 			else if(up1&&!down1){
 			  let variable=(parseInt((document.getElementById("rating1")).innerHTML)+2).toString();
 			  document.getElementById("rating1").innerHTML =variable;
@@ -78,6 +79,8 @@ document.addEventListener('DOMContentLoaded', function () {
 			  up1=false;
 			}
 			);
+			
+			//event listeners for upvote and downvote
 			document.getElementById("upvote2").addEventListener("click", function(){if(up2&&down2){
 				let variable=(parseInt((document.getElementById("rating2")).innerHTML)+1).toString();
 			  document.getElementById("rating2").innerHTML =variable;
@@ -93,6 +96,9 @@ document.addEventListener('DOMContentLoaded', function () {
 			}
 
 			);
+			
+			
+			//event listeners for upvote and downvote
 			document.getElementById("downvote1").addEventListener("click", function(){if(down1&&up1){
 				let variable=(parseInt((document.getElementById("rating1")).innerHTML)-1).toString();
 			  document.getElementById("rating1").innerHTML =variable;
@@ -108,6 +114,9 @@ document.addEventListener('DOMContentLoaded', function () {
 			down1=false;
 			}
 			);
+			
+			
+			//event listeners for upvote and downvote
 			document.getElementById("downvote2").addEventListener("click", function(){if(down2&&up2){
 				let variable=(parseInt((document.getElementById("rating2")).innerHTML)-1).toString();
 			  document.getElementById("rating2").innerHTML =variable;
@@ -131,17 +140,55 @@ document.addEventListener('DOMContentLoaded', function () {
 }
 );
 
+//rating sorting algorithm
 function sortFeedRate(postList){
 	let posts = postList.sort(function(a, b){return b.rating-a.rating});
 	return posts;
 }
-
+//time sorting algorithm
 function sortFeedTime(postList){
 	let posts = postList.sort(function(a, b){return b.time.getTime()-a.time.getTime()});
 	return posts;
 }
 
 
+//creates the post based on information in the form
+//todo get username and time of post from server
+function createPost(){
+	let name=document.getElementById("titleText").value;
+	let contents=document.getElementById("contentText").value;
+	return {"name":name,"contents":contents,"rating":0,"author":getUsername(),"tag":getTags(),"time":getServerTime()};
+}
+
+//todo actual server functionality
+function getServerTime(){
+	return "time";
+}
+
+//membership functionality
+function getUsername(){
+	return "User";
+}
+
+//gets tags from form as array
+function getTags(){
+	let array=[];
+	for(let i=0;i<tagAmount;i++){
+		var button=document.getElementById("tag"+i);
+
+		if(button.checked===true){
+			array.push(button.name);
+			console.log(button.name);
+	}
+}
+return array;
+}
+
+
+
+
+//gets the autogenerated posts
+//todo get posts from server
 function getPosts(){
 	return [{"name":"Educational Resources","contents":"Five professors of San Jose State University have proposed a new tutoring program for low-income and homeless students.","rating":3,"author":"author???","tag":"meh","time": new Date('Sat Apr 27 2019 7:51:16 GMT-0700 (Pacific Daylight Time)'),},
 	{"name":"New Options for Food","contents":"Two new food banks have opened up near San Jose State University;  one in 7th Street an one in San Fernando Street.","rating":123,"author":"author???2","tag":"meh2","time":new Date('Sat Apr 27 2019 11:51:16 GMT-0700 (Pacific Daylight Time)'),},
@@ -149,6 +196,8 @@ function getPosts(){
 	}]
 }
 
+//displays posts
+//todo push posts to server
 function pushPost(post){
 	alert(JSON.stringify(post));
 }
