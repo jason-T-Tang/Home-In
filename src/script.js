dcf5f2ddc0f2d9f4c466b3e9ac5b58f951c84912
DeadlyPho <jasontang.exe@gmail.com>
2019-04-28 10:08:36 -0700

    My version of the button algorithm

--- src/script.js
+++ src/script.js
@@ -62,46 +62,7 @@ document.getElementById("downvote2").addEventListener("click", function(){if(dow
 
 }
 );
-/*
-document.addEventListener('DOMContentLoaded', function () {
-	let posts= feed()
-	let sortedArray = sortFeed(posts);
-	//var canvas = document.getElementById("myCanvas");
-	//var ctx = canvas.getContext("2d");
 
-	let padding=10;
-	for(let i=0;i<sortedArray.length;i=i+1){
-		//var Mycanvas = document.getElementById("myCanvas");
-		var canvas=document.createElement("CANVAS");
-		canvas.id="post"+i;
-		let iterX=20;
-		let iterY=30;
-		//canvas.className  = "myClass";
-		//canvas.id = "myId";
-		//canvas.width=100;
-		//canvas.height=100;
-		var ctx = canvas.getContext("2d");
-		ctx.rect(iterX-5, iterY-10, 100, 55);
-		ctx.stroke();
-		ctx.font = "10px Arial";
-		ctx.fillText("Title: "+sortedArray[i].name, iterX, iterY);
-		iterY=iterY+10;
-		ctx.fillText("Content: "+sortedArray[i].contents, iterX, iterY);
-		iterY=iterY+10;
-		ctx.fillText("Rating: "+sortedArray[i].rating, iterX, iterY);
-		iterY=iterY+10;
-		ctx.fillText("Author: "+sortedArray[i].author, iterX, iterY);
-		iterY=iterY+10;
-		ctx.fillText("Tag: "+sortedArray[i].tag, iterX, iterY);
-		iterY=iterY+10+padding;
-		//document.getElementsByTagName('body')[0].appendChild(canvas);
-		document.body.appendChild(canvas)
-				
-	}
-	
-}
-);
-*/
 function sortFeed(postList){
 	let posts = postList.sort(function(a, b){return b.rating-a.rating});
 	return posts;
